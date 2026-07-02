13089ba59cfc05ff93224d7ca7f74665d4ca505e
Ivan Melo <ivanmelo@ijaxxa.com>
2026-07-01 20:07:18 -0600

    Cleaner device connection.

--- build/CMakeFiles/Makefile.cmake
+++ build/CMakeFiles/Makefile.cmake
@@ -46,5 +46,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/MotionPlusPlus.dir/DependInfo.cmake"
+  "CMakeFiles/motionplusplus.dir/DependInfo.cmake"
   )

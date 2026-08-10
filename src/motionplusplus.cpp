f014c4c96ad569536374d7b7d88bb4d8bfc7c12f
Ivan Melo <ivanmelo@ijaxxa.com>
2026-08-10 12:25:16 -0600

    Added service.

--- src/motionplusplus.cpp
+++ src/motionplusplus.cpp
@@ -1,17 +1,30 @@
+#include <ostream>
 #include <print>
+#include <iostream>
 #include <chrono>
 //#include "libmotionplusplus/WiiMote.hpp"
 #include "libmotionplusplus/ControllerManager.hpp"
+#include <csignal>
+#include <atomic>
 
 using std::println;
+using std::signal;
+using std::cout;
 
 using namespace std::chrono_literals;
 using namespace motionplusplus;
 
+std::atomic<bool> running{true};
+
+void quitHandle(int) { running = false; }
+
 int main () {
+    signal(SIGTERM, quitHandle);
+    signal(SIGINT, quitHandle);
+
     ControllerManager cm;
 
-    while (true) {
+    while (running) {
         auto up = cm.update(10ms);
 
         if (cm.isNewContrllers()) {
@@ -19,18 +32,21 @@ int main () {
             for (auto &ctrl_id : cm.getActiveControllers()) {
                 println("{}", *(cm.getController(ctrl_id)));
             }
+            cout.flush();
         }
 
         if (!up) {
-            if (up.error().value() == EAGAIN) continue;
+            if (up.error().value() == EAGAIN || up.error().value() == EINTR) continue;
             if (up.error().value() == POLLHUP) {
                 println("Device disconnected.");
                 for (auto &ctrl_id : cm.getActiveControllers()) {
                     println("{}", *(cm.getController(ctrl_id)));
                 }
+                cout.flush();
                 continue;
             }
             println("Update error: {}", up.error().message());
+            cout.flush();
             return 1;
         }
     }
